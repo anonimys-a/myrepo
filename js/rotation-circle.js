ce9323dbda9054d46dcf9209d3b33b4508d8bd4b
anonimus <git.projects.for.me@gmail.com>
2016-12-09 02:17:09 +0100

    change the circle implementation

--- js/rotation-circle.js
+++ js/rotation-circle.js
@@ -1,42 +1,32 @@
 $(document).ready(function() {
-	var nbOptions = 8;
-	var angleStart = -360;
-
-	// jquery rotate animation
-	function rotate(li,d) {
-	    $({d:angleStart}).animate({d:d}, {
-	        step: function(now) {
-	            $(li)
-	               .css({ transform: 'rotate('+now+'deg)' })
-	               .find('label')
-	                  .css({ transform: 'rotate('+(-now)+'deg)' });
-	        }, duration: 0
-	    });
-	}
-
-	// show / hide the options
-	function toggleOptions(s) {
-	    $(s).toggleClass('open');
-	    var li = $(s).find('li');
-	    var deg = $(s).hasClass('half') ? 180/(li.length-1) : 360/li.length;
-	    for(var i=0; i<li.length; i++) {
-	        var d = $(s).hasClass('half') ? (i*deg)-90 : i*deg;
-	        $(s).hasClass('open') ? rotate(li[i],d) : rotate(li[i],angleStart);
-	    }
-	}
-
-	$('.selector .main-member-in-circle').click(function(e) {
-		e.preventDefault();
-	    var showElementId = this.id;
-	    $(".about-us-biografy").html($("." + showElementId).html());
+	var type = 1, //circle type - 1 whole, 0.5 half, 0.25 quarter
+	radius = '12em', //distance from center
+	start = -90, //shift start from 0
+	$elements = $('li.circle-list-item:not(:first-child)'),
+	numberOfElements = (type === 1) ?  $elements.length : $elements.length - 1, //adj for even distro of elements when not full circle
+	slice = 360 * type / numberOfElements;
+	
+	$elements.each(function(i) {
+		var $self = $(this),
+		    rotate = slice * i + start,
+		    rotateReverse = rotate * -1;
+		
+		$self.css({
+		    'transform': 'rotate(' + rotate + 'deg) translate(' + radius + ') rotate(' + rotateReverse + 'deg)'
+		});
 	});
 
-	setTimeout(function() { toggleOptions('.selector'); }, 100);//@ sourceURL=pen.js
-	
-	//show bio for every person
-	$('div.selector input').click(function(e) {
+	$('li.main-member-in-circle').click(function(e) {
 		e.preventDefault();
 	    var showElementId = this.id;
-	    $(".about-us-biografy").html($("." + showElementId).html());
+	    $(".team-member-biografy").html($("." + showElementId).html());
+	});
+
+	
+	//show bio for every person
+	$('li.circle-list-item').click(function(e) {
+		e.preventDefault();
+	    var showElementId = this.id;
+	    $(".team-member-biografy").html($("." + showElementId).html());
 	});
 });

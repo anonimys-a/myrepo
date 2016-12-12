e0ff6ec71057ae93b636f60f0efcc18863df3e5a
anonimus <git.projects.for.me@gmail.com>
2016-12-13 00:45:08 +0100

    add modal dialog on click for every person in the circle

--- js/rotation-circle.js
+++ js/rotation-circle.js
@@ -76,5 +76,13 @@ $(document).ready(function() {
 		e.preventDefault();
 	    var showElementId = this.id;
 	    $(".team-member-biografy").html($("." + showElementId).html());
+	    //if we want to show text in modal dialog then this is the code for that
+	    $('#myModal').modal({
+	        show: 'true'
+	    }); 
+	    //set the modal body 
+	    $(".modal-body").text($("." + showElementId + ' p').text()); 
+	    //set the modal header
+	    $(".modal-header").text($("." + showElementId + ' h2').text()); 
 	});
 });
